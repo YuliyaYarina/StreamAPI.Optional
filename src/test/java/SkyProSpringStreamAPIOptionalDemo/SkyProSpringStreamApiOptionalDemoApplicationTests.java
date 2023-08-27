cea283df4dee3179bd8a202ca2940813bf405cf1
Юлия <Yulechka140200@gmail.com>
2023-08-28 02:19:46 +0500

    библиотека

---
+++ src/test/java/SkyProSpringStreamAPIOptionalDemo/SkyProSpringStreamApiOptionalDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package SkyProSpringStreamAPIOptionalDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProSpringStreamApiOptionalDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
